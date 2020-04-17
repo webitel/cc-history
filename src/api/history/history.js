d3f4c3c10df95398cb0b8c33e7076201bdd023c3
dlohvinov <nslbnr@gmail.com>
2020-04-17 13:48:46 +0300

    dev: added date time response formatters [WTEL-1160]

--- src/api/history/history.js
+++ src/api/history/history.js
@@ -5,12 +5,27 @@ import store from '../../store/index';
 
 const callService = new CallServiceApiFactory(configuration, '', instance);
 
+const computeDate = (createdAt) => {
+  const date = new Date(+createdAt);
+  return date.toLocaleDateString();
+};
+
+const computeTime = (createdAt) => {
+  const date = new Date(+createdAt);
+  return date.toLocaleTimeString();
+};
+
 const formatResponse = (response) => {
   const defaultObject = {
     _isSelected: false,
   };
   if (response.items) {
-    return response.items.map((item) => ({ ...defaultObject, ...item }));
+    return response.items.map((item) => ({
+      ...defaultObject,
+      ...item,
+      date: computeDate(item.createdAt),
+      time: computeTime(item.createdAt),
+    }));
   }
   return [];
 };
