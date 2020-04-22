cca0edc3a48e954c1869796a96fcefdbffcb1890
Daniil Lohvinov <nsblnr@gmail.com>
2020-04-22 15:15:35 +0300

    dev: added single item file download [WTEL-1175]

--- src/components/history/history-main/the-history-main.vue
+++ src/components/history/history-main/the-history-main.vue
@@ -65,7 +65,11 @@
           @play="play"
         ></media-select>
 
-        <button class="icon-btn table-action" @click.prevent.stop="download">
+        <button
+          v-if="item.files"
+          class="icon-btn table-action"
+          @click.prevent.stop="downloadItemFiles(item.files)"
+        >
           <icon>
             <svg class="icon icon-download_md md">
               <use xlink:href="#icon-download_md"></use>
@@ -122,11 +126,12 @@
   import { getHistory } from '../../../api/history/history';
   import sortFilterMixin from '../../../mixins/filters/sortFilterMixin/sortFilterMixin';
   import loadHistoryMixin from '../../../mixins/loadHistory/loadHistoryMixin';
-  import mediaMixin from '../../../mixins/mediaMixin';
+  import mediaMixin from '../../../mixins/files/mediaMixin';
+  import downloadFileMixin from '../../../mixins/files/downloadFiles/downloadFileMixin';
 
   export default {
     name: 'the-history-main',
-    mixins: [loadHistoryMixin, sortFilterMixin, mediaMixin],
+    mixins: [loadHistoryMixin, sortFilterMixin, mediaMixin, downloadFileMixin],
     components: {
       GridTable,
       FilterFields,
---
+++ src/mixins/files/downloadFiles/downloadFileMixin.js
@@ -0,0 +1,26 @@
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+import { fetchFileBinary } from '../filesScripts';
+import eventBus from '../../../utils/eventBus';
+
+const zip = new JSZip();
+
+export default {
+  methods: {
+    async downloadItemFiles(files) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const file of files) {
+        // eslint-disable-next-line no-await-in-loop
+        const binary = await fetchFileBinary(file.id);
+        const ext = file.mimeType.split('/').pop();
+        zip.file(`${file.name}.${ext}`, binary);
+      }
+      try {
+        const file = await zip.generateAsync({ type: 'blob' });
+        saveAs(file, 'files.zip');
+      } catch (err) {
+        eventBus.$emit('notificationError', 'Failed to load the file');
+      }
+    },
+  },
+};
