fb31559d894310af4ffc77b4d78b8d4636658912
Daniil Lohvinov <nsblnr@gmail.com>
2020-04-23 13:18:35 +0300

    refactor: filter search dedicated as a separate component [WTEL-829]

---
+++ src/components/history/history-heading/filters/filter-search.vue
@@ -0,0 +1,34 @@
+<template>
+  <search
+    v-model="value"
+    @search="setQueryValue({ filterQuery, value })"
+  />
+</template>
+
+<script>
+  import Search from '../../../utils/search-input.vue';
+  import valueFilterMixin from '../../../../mixins/filters/valueFilterMixin';
+
+  export default {
+    name: 'filter-search',
+    mixins: [valueFilterMixin],
+    components: {
+      Search,
+    },
+
+    data: () => ({
+      value: '',
+      filterQuery: 'search',
+    }),
+
+    methods: {
+      restoreValue({ value }) {
+        this.value = value;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
