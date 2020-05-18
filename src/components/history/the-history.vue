bfd7ea506ffbc9788e5264c9d7fe9d900822cef5
Daniil Lohvinov <nsblnr@gmail.com>
2020-05-18 16:19:01 +0300

    dev: added header, app navigator and preferences components from workspace, fixed ui [WTEL-1217]

--- src/components/history/the-history.vue
+++ src/components/history/the-history.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="body">
+    <cc-header/>
     <main class="history-container">
       <notification/>
       <history-heading/>
@@ -11,6 +12,7 @@
 
 <script>
   import Notification from '../utils/notification.vue';
+  import CcHeader from '../cc-header/cc-header.vue';
   import HistoryHeading from './history-heading/the-history-heading.vue';
   import HistoryFilters from './history-filters/the-history-filters.vue';
   import HistoryMain from './history-main/the-history-main.vue';
@@ -19,6 +21,7 @@
     name: 'the-history',
     components: {
       Notification,
+      CcHeader,
       HistoryHeading,
       HistoryFilters,
       HistoryMain,
@@ -27,7 +30,14 @@
 </script>
 
 <style lang="scss" scoped>
+  .body {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
   .history-container {
+    flex-grow: 1;
     display: flex;
     flex-direction: column;
     padding: calcRem(20px) calcRem(30px) calcRem(60px);
