321b655bdd8ae7f845abd87587d703802c1344fc
Daniil Lohvinov <nsblnr@gmail.com>
2020-05-20 17:01:23 +0300

    dev: grid actions moved to separate components [WTEL-1235]

---
+++ src/components/history/history-main/grid-templates/grid-download-action.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="icon-btn">
+    <icon>
+      <svg class="icon icon-download_md md">
+        <use xlink:href="#icon-download_md"></use>
+      </svg>
+    </icon>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'grid-download-action',
+  };
+</script>
+
+<style scoped>
+
+</style>
