afcf5b23f6b600525f3c9475a06524182cf87c6b
Daniil Lohvinov <nsblnr@gmail.com>
2020-06-04 17:32:02 +0300

    dev: added single datepicker component, from-to filter refactored [WTEL-1274]
    from-to filter refactored to use 2 single components instead of one doubled

---
+++ src/components/filters/filter-from.vue
@@ -0,0 +1,37 @@
+<template>
+  <dt-picker
+    v-model="value"
+    :label="'From'"
+    :preview-label="'Date & Time:'"
+    @input="setQueryValue({ filterQuery, value: `${$event}` })"
+  ></dt-picker>
+</template>
+
+<script>
+  import DtPicker from '../utils/datetimepicker.vue';
+  import valueFilterMixin from '../../mixins/filters/valueFilterMixin';
+  import getTodayStart from '../../utils/getTodayStart';
+
+  export default {
+    name: 'filter-from',
+    mixins: [valueFilterMixin],
+    components: {
+      DtPicker,
+    },
+
+    data: () => ({
+      value: getTodayStart(),
+      filterQuery: 'from',
+    }),
+
+    methods: {
+      restoreValue({ value }) {
+        this.value = +value;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/components/history/history-filters/the-history-filters.vue
+++ src/components/history/history-filters/the-history-filters.vue
@@ -1,79 +1,80 @@
 <template>
-  <section class="history-section history-filters">
+  <section class="history-section history-filters-section">
     <filter-fields
       v-show="isFilterFieldsOpened"
       @close="isFilterFieldsOpened = false"
     ></filter-fields>
-    <form class="history-filters__filters">
+    <form class="history-filters">
       <!--      dt picker-->
-      <div class="col-md-12 col-lg-8 col-xl-6">
-        <filter-datetime/>
-      </div>
+<!--      <div class="col-md-12 col-lg-8 col-xl-6">-->
+        <filter-from/>
+        <filter-to/>
+<!--      </div>-->
       <!--      [ARRAY] type multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{
-          'd-md-none': !isOpened,
-          'd-lg-block': !isOpened,
-        }"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{-->
+<!--          'd-md-none': !isOpened,-->
+<!--          'd-lg-block': !isOpened,-->
+<!--        }"-->
+<!--      >-->
         <filter-type/>
-      </div>
+<!--      </div>-->
       <!--      [ARRAY] direction multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{
-                'd-md-none': !isOpened,
-                'd-lg-none': !isOpened,
-                'd-xl-block': !isOpened
-              }"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{-->
+<!--                'd-md-none': !isOpened,-->
+<!--                'd-lg-none': !isOpened,-->
+<!--                'd-xl-block': !isOpened-->
+<!--              }"-->
+<!--      >-->
         <filter-direction/>
-      </div>
+<!--      </div>-->
       <!--     [ARRAY] [API FETCH] user multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{
-                'd-md-none': !isOpened,
-              }"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{-->
+<!--                'd-md-none': !isOpened,-->
+<!--              }"-->
+<!--      >-->
         <filter-user/>
-      </div>
+<!--      </div>-->
       <!--      [ARRAY] [API FETCH] gateway multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{'d-md-none': !isOpened}"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{'d-md-none': !isOpened}"-->
+<!--      >-->
         <filter-gateway/>
-      </div>
+<!--      </div>-->
       <!--      [ARRAY] [API FETCH] agent multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{'d-md-none': !isOpened}"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{'d-md-none': !isOpened}"-->
+<!--      >-->
         <filter-agent/>
-      </div>
+<!--      </div>-->
       <!--     [ARRAY]  [API FETCH] team multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{'d-md-none': !isOpened}"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{'d-md-none': !isOpened}"-->
+<!--      >-->
         <filter-team/>
-      </div>
+<!--      </div>-->
       <!--    [ARRAY]   [API FETCH] queue multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{'d-md-none': !isOpened}"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{'d-md-none': !isOpened}"-->
+<!--      >-->
         <filter-queue/>
-      </div>
+<!--      </div>-->
       <!--      duration inputs -->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{'d-md-none': !isOpened}"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{'d-md-none': !isOpened}"-->
+<!--      >-->
         <filter-duration/>
-      </div>
+<!--      </div>-->
       <!--     [ARRAY]  tags multi select-->
       <!--      <div-->
       <!--        class="col-md-6 col-lg-4 col-xl-3"-->
@@ -82,19 +83,18 @@
       <!--        <filter-tags/>-->
       <!--      </div>-->
       <!--     [ARRAY]  hangup cause multi select-->
-      <div
-        class="col-md-6 col-lg-4 col-xl-3"
-        :class="{'d-md-none': !isOpened}"
-      >
+<!--      <div-->
+<!--        class="col-md-6 col-lg-4 col-xl-3"-->
+<!--        :class="{'d-md-none': !isOpened}"-->
+<!--      >-->
         <filter-cause/>
-      </div>
+<!--      </div>-->
     </form>
     <table-actions
       settings
+      refresh
       column-select
       filter-reset
-      export-btn
-      import-btn
       :is-settings-active="isOpened"
       @settings="expandFilters"
       @refresh="refreshData"
@@ -109,7 +109,8 @@
 <script>
   import { mapActions } from 'vuex';
   import FilterFields from '../../filters/filter-table-fields.vue';
-  import FilterDatetime from '../../filters/filter-datetime.vue';
+  import FilterFrom from '../../filters/filter-from.vue';
+  import FilterTo from '../../filters/filter-to.vue';
   import FilterType from '../../filters/filter-type.vue';
   import FilterDirection from '../../filters/filter-direction.vue';
   import FilterUser from '../../filters/filter-user.vue';
@@ -126,7 +127,8 @@
     name: 'the-history-filters',
     components: {
       FilterFields,
-      FilterDatetime,
+      FilterFrom,
+      FilterTo,
       FilterType,
       FilterDirection,
       FilterUser,
@@ -173,33 +175,41 @@
 <style lang="scss" scoped>
   @import "../../../css/utils/bootstrap-grid.css";
 
-  .history-filters {
+  .history-section.history-filters-section {
     display: flex;
     align-items: flex-start;
-    padding: (18px) (28px);
-    margin: (20px) 0;
-
-    &__filters {
-      flex: 1 1 auto;
-      display: flex;
-      flex-wrap: wrap;
-
-      .dt-picker {
-        width: 100%;
-      }
-
-      .hs-multiselect {
-        width: 100%;
-      }
-
-      .dt-picker, .hs-multiselect {
-        /*margin-right: (50px);*/
-        margin-bottom: (18px);
-      }
-    }
+    padding: 18px 28px 0;
+    margin: 20px 0;
 
     .table-actions {
-      margin-top: 48px;
+      margin-top: 24px;
+    }
+  }
+
+  .history-filters {
+    flex: 1 1 auto;
+    /*display: flex;*/
+    /*flex-wrap: wrap;*/
+    display: grid;
+    grid-template-columns: repeat(auto-fill,  minmax(300px, 1fr));
+    grid-template-rows: repeat(1, 1fr);
+    /*grid-auto-rows: 0; !* set height to 0 for autogenerated grid rows *!*/
+    /*overflow: hidden; !* hide grid items that overflow *!*/
+    grid-column-gap: 20px;
+
+    .dt-picker {
+      width: 100%;
+      /*width: 596px;*/
+    }
+
+    .hs-multiselect {
+      width: 100%;
+      /*width: 271px;*/
+    }
+
+    .dt-picker, .hs-multiselect {
+      /*margin-right: (50px);*/
+      margin-bottom: (18px);
     }
   }
 </style>
