3e907e6cf57268b767fe99471db6990faca72ce9
dlohvinov <nsblnr@gmail.com>
2020-09-04 12:00:32 +0300

    fix: fixed agents, queues and teams api filters [WTEL-1383]

--- src/shared/filters/api/AgentsAPIRepository.js
+++ src/shared/filters/api/AgentsAPIRepository.js
@@ -6,44 +6,46 @@ import { formatOptions, defaultParams } from './defaults/defaults';
 const agentService = new AgentServiceApiFactory(configuration, '', instance);
 
 export const fetchAgents = async ({
-                                    page,
-                                    size,
-                                    search,
-                                    fields,
-                                    id,
+                                      page,
+                                      size,
+                                      search,
+                                      fields,
+                                      id,
                                   }) => {
-  try {
-    const response = await agentService.searchAgent(
-      page,
-      size,
-      `${search}*`,
-      undefined,
-      fields,
-      null,
-      id,
-    );
-    return formatOptions(response);
-  } catch (err) {
-    throw err;
-  }
+    try {
+        // eslint-disable-next-line no-param-reassign
+        if (search && search.slice(-1) !== '*') search += '*';
+        const response = await agentService.searchAgent(
+            page,
+            size,
+            search,
+            undefined,
+            fields,
+            null,
+            id,
+        );
+        return formatOptions(response);
+    } catch (err) {
+        throw err;
+    }
 };
 
 const AgentsAPIRepository = {
-  getAgents(argParams) {
-    const params = {
-      ...defaultParams,
-      ...argParams,
-    };
-    return fetchAgents(params);
-  },
-  getAgentsByIds(idList) {
-    const params = {
-      ...defaultParams,
-      size: idList.length,
-      id: idList,
-    };
-    return fetchAgents(params);
-  },
+    getAgents(argParams) {
+        const params = {
+            ...defaultParams,
+            ...argParams,
+        };
+        return fetchAgents(params);
+    },
+    getAgentsByIds(idList) {
+        const params = {
+            ...defaultParams,
+            size: idList.length,
+            id: idList,
+        };
+        return fetchAgents(params);
+    },
 };
 
 export default AgentsAPIRepository;
