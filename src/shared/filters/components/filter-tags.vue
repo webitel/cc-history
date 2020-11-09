bb4250cc68b3279368a1dce7bcf400d6e9781b02
dlohvinov <nsblnr@gmail.com>
2020-11-09 21:06:06 +0200

    dev: added tags history filter [WTEL-1516]

---
+++ src/shared/filters/components/filter-tags.vue
@@ -0,0 +1,31 @@
+<template>
+  <wt-select
+    v-model="value"
+    :options="options"
+    :label="$t('fields.tags')"
+    :track-by="trackBy"
+    multiple
+    @reset="setValueToQuery({ value, filterQuery, storedProp })"
+    @closed="setValueToQuery({ value, filterQuery, storedProp })"
+  ></wt-select>
+</template>
+
+<script>
+  import enumFilterMixin from '@webitel/ui-sdk/src/mixins/dataFilterMixins/enumFilterMixin';
+  import TagOptions from '../api/TagOptions.enum';
+
+  export default {
+    name: 'filter-tags',
+    mixins: [enumFilterMixin],
+
+    data: () => ({
+      options: TagOptions,
+      filterQuery: 'tags',
+      storedProp: 'value',
+    }),
+  };
+</script>
+
+<style scoped>
+
+</style>
