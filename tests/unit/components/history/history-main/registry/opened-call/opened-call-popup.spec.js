264ae1a0abac33e70b890691fb557c767fe2b1d3
dlohvinov <nsblnr@gmail.com>
2020-11-12 18:48:55 +0200

    fix: added opened-call-popup switch to default tab if opened call changes [WTEL-1530]

--- tests/unit/components/history/history-main/registry/opened-call/opened-call-popup.spec.js
+++ tests/unit/components/history/history-main/registry/opened-call/opened-call-popup.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex';
-import OpenedCallPopup from '../../../../../../../src/components/history/history-main/registry/opened-call/opened-call-popup.vue';
+import OpenedCallPopup
+  from '../../../../../../../src/components/history/history-main/registry/opened-call/opened-call-popup.vue';
 import openedCall from '../../../../../../../src/store/modules/registry/opened-call/opened-call';
 
 const localVue = createLocalVue();
@@ -22,8 +23,31 @@ describe('Opened call popup (wrapper)', () => {
 
   it('renders a component if main call has any children', () => {
     const wrapper = shallowMount(OpenedCallPopup, {
-      localVue, store, computed: { mainCall() { return { hasChildren: true }; } },
+      localVue, store, computed: {
+        mainCall() {
+          return { hasChildren: true };
+        },
+      },
     });
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('resets currentTab to call-info, if mainCall changes', async () => {
+    const currentTab = { value: 'call-legs' };
+    const callInfoTab = { value: 'call-info' };
+    const wrapper = shallowMount(OpenedCallPopup, {
+      localVue,
+      store,
+      data: () => ({ mainCallTrigger: false, currentTab }),
+      computed: {
+        mainCall() {
+          return { mainCallTrigger: this.mainCallTrigger, hasChildren: true };
+        },
+      },
+    });
+    expect(wrapper.vm.currentTab).toEqual(currentTab);
+    wrapper.setData({ mainCallTrigger: true });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.currentTab).toEqual(callInfoTab);
   });
 });
