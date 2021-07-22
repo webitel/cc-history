d82bcc062f70b17617bda485a9c5c59116e5debc
dlohvinov <nsblnr@gmail.com>
2021-07-22 18:07:06 +0300

    test: fixed history tests [WTEL-1946]

--- tests/unit/components/history/history-main/dashboards/filters/filters.spec.js
+++ tests/unit/components/history/history-main/dashboards/filters/filters.spec.js
@@ -1,15 +1,19 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils';
+import VueRouter from 'vue-router';
 import Vuex from 'vuex';
 import FilterInterval from '../../../../../../../src/components/history/history-main/dashboards/filters/filter-interval/filter-interval.vue';
 import store from '../../../../../../../src/store/index';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(VueRouter);
+
+const router = new VueRouter();
 
 describe('Dashboards: filter components', () => {
   it('renders interval filter component', () => {
     const wrapper = shallowMount(FilterInterval, {
-      localVue, store, mocks: { $route: { query: {} } },
+      localVue, store, router,
     });
     expect(wrapper.findComponent({ name: 'wt-select' }).element).toBeVisible();
   });
