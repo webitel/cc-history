0cd35c4a6c2dc280caac04d030182e4ffc305bd9
Vasyl <vasjanja@gmail.com>
2021-11-11 17:17:12 +0200

    feature: comment form created [WPRO-218]

---
+++ src/components/history/history-main/registry/opened-call/opened-call-tabs/opened-call-comment-form.vue
@@ -0,0 +1,93 @@
+<template>
+  <section class="comment-form">
+    <wt-timepicker
+      class="comment-form__timepicker"
+      label="Begin"
+      v-model="comment.startSec"
+    ></wt-timepicker>
+    <wt-timepicker
+      class="comment-form__timepicker"
+      label="End"
+      v-model="comment.endSec"
+    ></wt-timepicker>
+    <div class="comment-form-textarea">
+      <wt-textarea
+        class="comment-form-textarea__textarea"
+        :class="{'comment-form-textarea--large': largeTextarea}"
+        v-model="comment.note"
+        label="Note"
+      ></wt-textarea>
+      <wt-icon-btn
+        class="comment-form-textarea--expand"
+        icon="expand"
+        size="sm"
+        @click="expandTextarea"
+      ></wt-icon-btn>
+    </div>
+    <wt-button class="comment-form__button" @click="saveComment">Save</wt-button>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'opened-call-comment-form',
+  data: () => ({
+    comment: {
+      note: '',
+      startSec: 0,
+      endSec: 0,
+    },
+    largeTextarea: false,
+  }),
+  methods: {
+    expandTextarea() {
+      this.largeTextarea = !this.largeTextarea;
+    },
+    saveComment() {
+      console.log('saved');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.comment-form::v-deep {
+  display: flex;
+  align-items: flex-start;
+  gap: var(--component-spacing);
+
+  .comment-form__timepicker {
+    flex-grow: 0;
+    flex-basis: auto;
+  }
+
+  .comment-form-textarea {
+    flex-grow: 1;
+    position: relative;
+
+    &__textarea {
+      .wt-textarea__textarea {
+        min-height: 0;
+        height: 38px;
+      }
+    }
+
+    &--large {
+      .wt-textarea__textarea {
+        height: 150px;
+      }
+    }
+
+    &--expand {
+      position: absolute;
+      right: 30px;
+      top: 38px
+    }
+  }
+
+  .comment-form__button {
+    margin-top: 26px;
+  }
+}
+</style>
