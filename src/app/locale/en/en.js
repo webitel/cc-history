abfa65c997cca8d71b50938e19192135776e9e2f
synoratska <m.synoratska@gmail.com>
2022-10-03 15:53:16 +0300

    fix:spelling mistake fixed[WTEL-2937]

--- src/app/locale/en/en.js
+++ src/app/locale/en/en.js
@@ -47,7 +47,7 @@ export default {
         wave: 'Call Wave',
       },
       stt: {
-        noTranscript: 'There is no transciption for this file',
+        noTranscript: 'There is no transcription for this file',
         transcribingInProgress: 'Transcription is in progress now. Please, refresh page in a minute.',
       },
     },
