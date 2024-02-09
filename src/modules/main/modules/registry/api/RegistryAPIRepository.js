ab559b5b004fe64f087abf764d152c08bc6bf4ba
Lera24 <bedulinavalera21@gmail.com>
2024-02-09 13:02:05 +0200

    fix: defaul value hasFile param in API[WTEL-4170]

--- src/modules/main/modules/registry/api/RegistryAPIRepository.js
+++ src/modules/main/modules/registry/api/RegistryAPIRepository.js
@@ -145,7 +145,7 @@ const getList = async (params) => {
       skip_parent: skipParent,
       parent_id: parentId,
       cause,
-      has_file: hasFile === undefined ? hasFile : hasFile === 'true', // str -> bool
+      has_file: hasFile,
       number: search,
       direction,
       id,
