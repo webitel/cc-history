5d34bc9e9c33b0a047020c1797b0f0e419f25b13
Олександр Пальонний <s2567k@gmail.com>
2025-02-04 21:05:42 +0200

    refactor: added validation for direction-filter, user-filter [WTEL-5922](https://webitel.atlassian.net/browse/WTEL-5922)

--- src/modules/filters/components/v2/filter-value-components/direction-filter.vue
+++ src/modules/filters/components/v2/filter-value-components/direction-filter.vue
@@ -3,6 +3,7 @@
     :clearable="false"
     :options="DirectionOptions"
     :value="model"
+    :v="v$.model"
     class="direction-filter"
     track-by="value"
     use-value-from-options-by-prop="value"
@@ -12,8 +13,27 @@
 
 <script lang="ts" setup>
 import DirectionOptions from '../../../enums/DirectionOptions.enum.js';
+import {useVuelidate} from "@vuelidate/core";
+import { required } from '@vuelidate/validators';
+import {computed, watch} from "vue";
 
 const model = defineModel<string>();
+
+const v$ = useVuelidate(computed(() => ({
+  model: {
+    required,
+  },
+})), { model }, { $autoDirty: true });
+
+v$.value.$touch();
+
+const emit = defineEmits<{
+  'update:invalid': [boolean];
+}>();
+
+watch(() => v$.value.$invalid, (invalid) => {
+  emit('update:invalid', invalid);
+}, { immediate: true });
 </script>
 
 <style lang="scss" scoped>
