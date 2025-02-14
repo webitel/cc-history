ec5de31ec303607f6f2be7e6d5fec6767cdc973a
Lera24 <bedulinavalera21@gmail.com>
2025-02-14 15:38:46 +0200

    feature: add new filters[WTEL-5917](https://webitel.atlassian.net/browse/WTEL-5917)

--- src/modules/filters/components/v2/the-history-filters.vue
+++ src/modules/filters/components/v2/the-history-filters.vue
@@ -71,53 +71,63 @@ import DynamicFilterAddAction
 import {FilterName} from '@webitel/ui-sdk/src/modules/Filters/v2/filters/types/Filter.d.ts';
 import DynamicFilterConfigForm
   from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/config/dynamic-filter-config-form.vue';
-import {useTableStore} from '../../../main/modules/registry/store/new/registry.store.ts';
-import DirectionFilter from './filter-value-components/direction-filter.vue';
-import CreatedAtFromFilter from './filter-value-components/created-at-from-filter.vue';
-import VariablesFilter from './filter-value-components/variables-filter.vue';
-import DurationFilter from './filter-value-components/duration-filter.vue';
-import UserFilter from './filter-value-components/user-filter.vue';
 import {SearchMode} from '../../../heading/modules/filters/enums/SearchMode.enum.ts';
 import SavePresetAction from "./presets/save-preset-action.vue";
-import UserFilterPreview
-  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/users/user-filter-value-preview.vue";
-import DirectionFilterPreview
-  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/directions/direction-filter-value-preview.vue";
-import DurationFilterPreview from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/durations/duration-filter-value-preview.vue";
+import {useTableStore} from '../../../main/modules/registry/store/new/registry.store.ts';
+
+import AgentFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/agent/agent-filter-value-field.vue';
+import AgentFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/agent/agent-filter-value-preview.vue';
+import AmdResultFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/amd-result/amd-result-filter-value-field.vue';
+import AmdResultFilterPreview
+  from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/amd-result/amd-result-filter-value-preview.vue';
+import ContactFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/contact/contact-filter-value-field.vue';
+import ContactFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/contact/contact-filter-value-preview.vue';
+import CreatedAtFromFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/created-at-from/created-at-from-filter-value-field.vue';
 import CreatedAtFromFilterPreview
   from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/created-at-from/created-at-from-filter-value-preview.vue";
-import VariablesFilterPreview
-  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/variables/variables-filter-value-preview.vue";
-import TeamFilterPreview
-  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/teams/team-filter-value-preview.vue";
-import TeamFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/teams/team-filter-value-field.vue';
-import TagFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/tags/tag-filter-value-field.vue';
-import TagFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/tags/tag-filter-value-preview.vue';
-import ScoreFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/scores/score-from-to-filter-value-field.vue';
-import ScoreFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/scores/score-from-to-filter-value-preview.vue';
-import RecordingFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/recordings/recording-filter-value-field.vue';
-import RecordingFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/recordings/recording-filter-value-preview.vue';
-import QueueFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/queues/queue-filter-value-field.vue';
-import QueueFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/queues/queue-filter-value-preview.vue';
-import HangupCauseFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/hangup-causes/hangup-cause-filter-value-field.vue';
+import CreatedAtToFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/created-at-to/created-at-to-filter-value-field.vue';
+import CreatedAtToFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/created-at-to/created-at-to-filter-value-preview.vue";
+import DirectionFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/direction/direction-filter-value-field.vue';
+import DirectionFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/direction/direction-filter-value-preview.vue";
+import EvaluationFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/evaluation/evaluation-filter-value-field.vue';
+import EvaluationFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/evaluation/evaluation-filter-value-preview.vue";
+import GatewayFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/gateway/gateway-filter-value-field.vue';
+import GatewayFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/gateway/gateway-filter-value-preview.vue';
+import GranteeFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/grantee/grantee-filter-value-field.vue';
+import GranteeFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/grantee/grantee-filter-value-preview.vue';
+import HangupCauseFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/hangup-cause/hangup-cause-filter-value-field.vue';
 import HangupCauseFilterPreview
-  from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/hangup-causes/hangup-cause-filter-value-preview.vue';
-import GranteeFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/grantees/grantee-filter-value-field.vue';
-import GranteeFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/grantees/grantee-filter-value-preview.vue';
-import GatewayFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/gateways/gateway-filter-value-field.vue';
-import GatewayFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/gateways/gateway-filter-value-preview.vue';
-import ContactFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/contacts/contacts-filter-value-field.vue';
-import ContactFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/contacts/contacts-filter-value-preview.vue';
-import CalendarFromToFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/calendar-from-to/calendar-from-to-filter-value-field.vue';
-import CalendarFromToFilterPreview
-  from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/calendar-from-to/calendar-from-to-filter-value-preview.vue';
-import AmdResultFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/amd-results/amd-result-filter-value-field.vue';
-import AmdResultFilterPreview
-  from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/amd-results/amd-result-filter-value-preview.vue';
-import AgentFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/agents/agent-filter-value-field.vue';
-import AgentFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/agents/agent-filter-value-preview.vue';
-
-// const props = defineProps({});
+  from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/hangup-cause/hangup-cause-filter-value-preview.vue';
+import QueueFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/queue/queue-filter-value-field.vue';
+import QueueFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/queue/queue-filter-value-preview.vue';
+import RatedByFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/rated-by/rated-by-filter-value-field.vue';
+import RatedByFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/rated-by/rated-by-filter-value-preview.vue';
+import RecordingFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/recording/recording-filter-value-field.vue';
+import RecordingFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/recording/recording-filter-value-preview.vue';
+import ScoreFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/score/score-from-to-filter-value-field.vue';
+import ScoreFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/score/score-from-to-filter-value-preview.vue';
+import TagFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/tag/tag-filter-value-field.vue';
+import TagFilterPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/tag/tag-filter-value-preview.vue';
+import TalkDuration from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/talk-duration/talk-duration-filter-value-field.vue';
+import TalkDurationPreview from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/talk-duration/talk-duration-filter-value-preview.vue';
+import TeamFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/team/team-filter-value-field.vue';
+import TeamFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/team/team-filter-value-preview.vue";
+import TotalDurationFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/total-duration/total-duration-filter-value-field.vue';
+import TotalDurationFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/total-duration/total-duration-filter-value-preview.vue";
+import TranscriptionFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/transcription/transcription-filter-value-field.vue';
+import TranscriptionFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/transcription/transcription-filter-value-preview.vue";
+import UserFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/user/user-filter-value-field.vue';
+import UserFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/user/user-filter-value-preview.vue";
+import VariableFilter from '@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/variable/variable-filter-value-field.vue';
+import VariableFilterPreview
+  from "@webitel/ui-sdk/src/modules/Filters/v2/filters/components/values/variable/variable-filter-value-preview.vue";
 
 const emit = defineEmits<{
   hide: [],
@@ -142,28 +152,40 @@ const unappliedFilters: Ref<Array<{ name: string, value: FilterName }>> = comput
 
   const filterOptions = [
     {
+      id: 'agent',
+      name: 'Agent title',
+    },
+    {
       id: 'amdResult',
       name: 'Amd Result title',
     },
     {
-      id: 'calendarFromTo',
-      name: 'calendarFromTo title',
+      id: 'contact',
+      name: 'contacts title',
     },
     {
-      id: 'grantee',
-      name: 'Grantee title',
+      id: 'createdAtFrom',
+      name: 'Created At From title',
+    },
+    {
+      id: 'createdAtTo',
+      name: 'Created At To title',
+    },
+    {
+      id: 'direction',
+      name: 'Direction title',
+    },
+    {
+      id: 'evaluation',
+      name: 'Evaluation title',
     },
     {
       id: 'gateway',
       name: 'Gateway title',
     },
     {
-      id: 'agent',
-      name: 'Agent title',
-    },
-    {
-      id: 'contact',
-      name: 'contacts title',
+      id: 'grantee',
+      name: 'Grantee title',
     },
     {
       id: 'hangupCause',
@@ -174,41 +196,46 @@ const unappliedFilters: Ref<Array<{ name: string, value: FilterName }>> = comput
       name: 'Queue title',
     },
     {
+      id: 'ratedBy',
+      name: 'ratedBy title',
+    },
+    {
       id: 'recording',
       name: 'Recording title',
     },
     {
-      id: 'direction',
-      name: 'Direction title',
-    },
-    {
-      id: 'duration',
-      name: 'Duration title',
-    },
-    {
-      id: 'user',
-      name: 'User title',
-    },
-    {
-      id: 'createdAtFrom',
-      name: 'Created At From title',
-    },
-    {
-      id: 'variables',
-      name: 'Vars title',
-    },
-    {
-      id: 'team',
-      name: 'Team',
+      id: 'score',
+      name: 'Score',
     },
     {
       id: 'tag',
       name: 'tag',
     },
     {
-      id: 'score',
-      name: 'Score',
-    }
+      id: 'talkDuration',
+      name: 'talkDuration title',
+    },
+    {
+      id: 'team',
+      name: 'Team',
+    },
+    {
+      id: 'totalDuration',
+      name: 'totalDuration title',
+    },
+    {
+      id: 'transcription',
+      name: 'transcription title',
+    },
+    {
+      id: 'user',
+      name: 'User title',
+    },
+
+    {
+      id: 'variable',
+      name: 'Vars title',
+    },
   ];
 
   return filterOptions;
@@ -216,80 +243,96 @@ const unappliedFilters: Ref<Array<{ name: string, value: FilterName }>> = comput
 
 const getFilterValueComponent = (filterName: FilterName) => {
   switch (filterName) {
-    case 'direction':
-      return DirectionFilter;
-    case 'createdAtFrom':
-      return CreatedAtFromFilter;
-    case 'variables':
-      return VariablesFilter;
-    case 'duration':
-      return DurationFilter;
-    case 'user':
-      return UserFilter;
-    case 'team':
-      return TeamFilter;
-    case 'tag':
-      return TagFilter;
-    case 'score':
-      return ScoreFilter;
-    case 'recording':
-      return RecordingFilter;
-    case 'queue':
-      return QueueFilter;
-    case 'hangupCause':
-      return HangupCauseFilter;
-    case 'grantee':
-      return GranteeFilter;
-    case 'gateway':
-      return GatewayFilter;
-    case 'contact':
-      return ContactFilter;
-    case 'calendarFromTo':
-      return CalendarFromToFilter;
-    case 'amdResult':
-      return AmdResultFilter;
     case 'agent':
       return AgentFilter;
+    case 'amdResult':
+      return AmdResultFilter;
+    case 'contact':
+      return ContactFilter;
+    case 'createdAtFrom':
+      return CreatedAtFromFilter;
+    case 'createdAtTo':
+      return CreatedAtToFilter;
+    case 'direction':
+      return DirectionFilter;
+    case 'evaluation':
+      return EvaluationFilter;
+    case 'gateway':
+      return GatewayFilter;
+    case 'grantee':
+      return GranteeFilter;
+    case 'hangupCause':
+      return HangupCauseFilter;
+    case 'queue':
+      return QueueFilter;
+    case 'ratedBy':
+      return RatedByFilter;
+    case 'recording':
+      return RecordingFilter;
+    case 'score':
+      return ScoreFilter;
+    case 'tag':
+      return TagFilter;
+    case 'talkDuration':
+      return TalkDuration;
+    case 'team':
+      return TeamFilter;
+    case 'totalDuration':
+      return TotalDurationFilter;
+    case 'transcription':
+      return TranscriptionFilter;
+    case 'user':
+      return UserFilter;
+    case 'variable':
+      return VariableFilter;
     default:
   }
 };
 
 const getPreviewComponent = (filterName: FilterName) => {
   switch (filterName) {
-    case 'direction':
-      return DirectionFilterPreview;
-    case 'createdAtFrom':
-      return CreatedAtFromFilterPreview;
-    case 'variables':
-      return VariablesFilterPreview;
-    case 'duration':
-      return DurationFilterPreview;
-    case 'user':
-      return UserFilterPreview;
-    case 'team':
-      return TeamFilterPreview;
-    case 'tag':
-      return TagFilterPreview;
-    case 'score':
-      return ScoreFilterPreview;
-    case 'recording':
-      return RecordingFilterPreview;
-    case 'queue':
-      return QueueFilterPreview;
-    case 'hangupCause':
-      return HangupCauseFilterPreview;
-    case 'grantee':
-      return GranteeFilterPreview;
-    case 'gateway':
-      return GatewayFilterPreview;
-    case 'contact':
-      return ContactFilterPreview;
-    case 'calendarFromTo':
-      return CalendarFromToFilterPreview;
-    case 'amdResult':
-      return AmdResultFilterPreview;
     case 'agent':
       return AgentFilterPreview;
+    case 'amdResult':
+      return AmdResultFilterPreview;
+    case 'contact':
+      return ContactFilterPreview;
+    case 'createdAtFrom':
+      return CreatedAtFromFilterPreview;
+    case 'createdAtTo':
+      return CreatedAtToFilterPreview;
+    case 'direction':
+      return DirectionFilterPreview;
+    case 'evaluation':
+      return EvaluationFilterPreview;
+    case 'gateway':
+      return GatewayFilterPreview;
+    case 'grantee':
+      return GranteeFilterPreview;
+    case 'hangupCause':
+      return HangupCauseFilterPreview;
+    case 'queue':
+      return QueueFilterPreview;
+    case 'ratedBy':
+      return RatedByFilterPreview;
+    case 'recording':
+      return RecordingFilterPreview;
+    case 'score':
+      return ScoreFilterPreview;
+    case 'tag':
+      return TagFilterPreview;
+    case 'talkDuration':
+      return TalkDurationPreview;
+    case 'team':
+      return TeamFilterPreview;
+    case 'totalDuration':
+      return TotalDurationFilterPreview;
+    case 'transcription':
+      return TranscriptionFilterPreview;
+    case 'user':
+      return UserFilterPreview;
+    case 'variable':
+      return VariableFilterPreview;
     default:
   }
 };
