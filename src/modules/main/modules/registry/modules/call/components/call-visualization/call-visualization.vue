ddb8216e3825fd6122ee473ea6f45aa461462d68
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2025-05-08 19:18:28 +0300

    feat: enhance audit-form components with improved styling and layout [WTEL-5679](https://webitel.atlassian.net/browse/WTEL-5679)

--- src/modules/main/modules/registry/modules/call/components/call-visualization/call-visualization.vue
+++ src/modules/main/modules/registry/modules/call/components/call-visualization/call-visualization.vue
@@ -21,7 +21,7 @@
 <script>
 import { useUserAccessControl } from '../../../../../../../../app/composables/useUserAccessControl';
 import CallTranscript from '../../../stt/components/call-page/call-transcript-section.vue';
-import CallAuditSection from '../../modules/evaluation/components/call-audit-section.vue';
+import CallAuditSection from '../../modules/call-audit/components/call-audit-section.vue';
 import CallWave from './wave/call-wave.vue';
 
 export default {
