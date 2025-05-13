7c6ace172d62368d5f328c07907d0378f7d2ceb2
Daniil Lohvinov <nsblnr@gmail.com>
2025-05-13 19:52:17 +0300

    cherry-picking feature, deleted some files here [WTEL-5679](https://webitel.atlassian.net/browse/WTEL-5679)

---
+++ src/modules/main/modules/registry/modules/call/modules/call-audit/components/rate/audit-rate-result.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="audit-rate-result">
+    <delete-confirmation-popup
+      :callback="deleteCallback"
+      :delete-count="deleteCount"
+      :shown="isConfirmationPopup"
+      @close="closeDelete"
+    />
+
+    <audit-rate-summary
+      :rate="rate"
+      @rate:edit="emit('rate:edit')"
+      @rate:delete="askDeleteConfirmation({ callback: () => emit('rate:delete') })"
+    />
+
+    <audit-rate-answers
+      :rate="rate"
+    />
+  </section>
+</template>
+
+<script lang="ts" setup>
+import DeleteConfirmationPopup
+  from '@webitel/ui-sdk/src/modules/DeleteConfirmationPopup/components/delete-confirmation-popup.vue';
+import {
+  useDeleteConfirmationPopup,
+} from '@webitel/ui-sdk/src/modules/DeleteConfirmationPopup/composables/useDeleteConfirmationPopup';
+import { EngineAuditRate } from 'webitel-sdk';
+
+import AuditRateAnswers from './answers/audit-rate-answers.vue';
+import AuditRateSummary from './summary/audit-rate-summary.vue';
+
+defineProps<{
+  rate: EngineAuditRate;
+}>();
+
+const emit = defineEmits<{
+  'rate:edit': [];
+  'rate:delete': [];
+}>();
+
+const {
+  isVisible: isConfirmationPopup,
+  deleteCount,
+  deleteCallback,
+  askDeleteConfirmation,
+  closeDelete,
+} = useDeleteConfirmationPopup();
+</script>
+
+<style lang="scss" scoped>
+
+.audit-rate-result {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: var(--spacing-sm);
+  gap: var(--spacing-sm);
+}
+</style>
---
+++ src/modules/main/modules/registry/modules/call/modules/call-audit/components/rate/summary/audit-rate-summary-scores.vue
@@ -0,0 +1,81 @@
+<template>
+  <section class="call-evaluation-scores">
+    <div
+      v-for="({ title, value, color }, index) in scores"
+      :key="index"
+      class="call-evaluation-score"
+    >
+      <wt-icon
+        icon="star--filled"
+        size="3xl"
+        :color="color"
+      />
+      <span class="call-evaluation-score__value">
+      {{ value.toFixed(2) }}
+    </span>
+      <span class="call-evaluation-score__title">
+      {{ title }}
+    </span>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const props = defineProps<{
+  scoreRequired: number | null;
+  scoreOptional: number | null;
+}>();
+
+const { t } = useI18n();
+
+const scores = computed(() => {
+  const scores = [];
+  if (props.scoreRequired != null) {
+    scores.push({
+      title: t('registry.call.evaluation.mandatory'),
+      value: props.scoreRequired,
+      color: 'success',
+    });
+  }
+
+  if (props.scoreOptional != null) {
+    scores.push({
+      title: t('registry.call.evaluation.optional'),
+      value: props.scoreOptional,
+      color: 'disabled',
+    });
+  }
+
+  return scores;
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@webitel/styleguide/typography' as *;
+
+.call-evaluation-scores {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: var(--spacing-sm);
+}
+
+.call-evaluation-score {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: var(--text-main-color);
+
+  &__value {
+    @extend %typo-subtitle-1;
+  }
+
+  &__title {
+    @extend %typo-body-1;
+  }
+}
+
+</style>
