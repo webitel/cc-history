48f4b25eefde18c6bc158e4f7aacc75ace0b5a48
volodymyr <volodymyr.dechtiaruk@gmail.com>
2026-02-11 12:40:13 +0200

    refactor: refactoring file formatting [WTEL-8271](https://webitel.atlassian.net/browse/WTEL-8271)

--- src/modules/heading/components/actions/history-download-action.vue
+++ src/modules/heading/components/actions/history-download-action.vue
@@ -18,93 +18,93 @@
 </template>
 
 <script>
-import { EngineCallFileType } from "@webitel/api-services/gen/models";
-import exportFilesMixin from "@webitel/ui-sdk/src/modules/FilesExport/mixins/exportFilesMixin";
+import { EngineCallFileType } from '@webitel/api-services/gen/models';
+import exportFilesMixin from '@webitel/ui-sdk/src/modules/FilesExport/mixins/exportFilesMixin';
 
-import APIRepository from "../../../../app/api/APIRepository";
-import { getCallMediaUrl } from "@webitel/api-services/api";
-import downloadTranscriptsMixin from "../../mixins/downloadTranscriptsMixin";
-import historyActionMixin from "../../mixins/historyActionMixin";
-import FilesCounter from "./files-counter.vue";
+import APIRepository from '../../../../app/api/APIRepository';
+import { getCallMediaUrl } from '@webitel/api-services/api';
+import downloadTranscriptsMixin from '../../mixins/downloadTranscriptsMixin';
+import historyActionMixin from '../../mixins/historyActionMixin';
+import FilesCounter from './files-counter.vue';
 
 export default {
-	name: "HistoryDownloadAction",
-	components: { FilesCounter },
-	mixins: [historyActionMixin, exportFilesMixin, downloadTranscriptsMixin],
-	props: {
-		dataList: {
-			type: Array,
-		},
-		filters: {
-			type: Object,
-		},
-	},
-	computed: {
-		selectedItems() {
-			return this.selected;
-		}, // needed for exportFilesMixin
-		isLoading() {
-			return this.isTranscriptsLoading || this.isFilesLoading;
-		},
-		downloadProgress() {
-			return this.transcriptDownloadProgress || this.filesDownloadProgress;
-		},
-		zippingProgress() {
-			return this.transcriptZippingProgress || this.filesZippingProgress;
-		},
-		downloadOptions() {
-			return [
-				{
-					value: "audio-recording",
-					text: this.$t("export.audioRecording", 2),
-					handler: this.callExportAudioFiles.bind(this),
-				},
-				{
-					value: "video-recording",
-					text: this.$t("export.videoRecording", 2),
-					handler: this.callExportVideoFiles.bind(this),
-				},
-				{
-					value: "screen-recording",
-					text: this.$t("export.screenRecording", 2),
-					handler: this.callExportScreenRecordings.bind(this),
-				},
-				{
-					value: "transcript",
-					text: this.$t("export.transcription", 2),
-					handler: this.exportTranscripts.bind(this),
-				},
-			];
-		},
-	},
-	created() {
-		this.initFilesExport({
-			fetchMethod: APIRepository.history.getHistory,
-			filename: "history-records",
-			filesURL: getCallMediaUrl,
-		});
-	},
-	methods: {
-		callExportAudioFiles() {
-			if (this.isLoading) return;
-			const params = { ...this.filters, hasFile: true };
-			return this.exportFiles(null, params, EngineCallFileType.FileTypeAudio);
-		},
-		callExportVideoFiles() {
-			if (this.isLoading) return;
-			const params = { ...this.filters, hasFile: true };
-			return this.exportFiles(null, params, EngineCallFileType.FileTypeVideo);
-		},
-		callExportScreenRecordings() {
-			if (this.isLoading) return;
-			const params = { ...this.filters, hasFile: true };
-			return this.exportFiles(
-				null,
-				params,
-				EngineCallFileType.FileTypeScreensharing,
-			);
-		},
-	},
+  name: 'HistoryDownloadAction',
+  components: { FilesCounter },
+  mixins: [historyActionMixin, exportFilesMixin, downloadTranscriptsMixin],
+  props: {
+    dataList: {
+      type: Array,
+    },
+    filters: {
+      type: Object,
+    },
+  },
+  computed: {
+    selectedItems() {
+      return this.selected;
+    }, // needed for exportFilesMixin
+    isLoading() {
+      return this.isTranscriptsLoading || this.isFilesLoading;
+    },
+    downloadProgress() {
+      return this.transcriptDownloadProgress || this.filesDownloadProgress;
+    },
+    zippingProgress() {
+      return this.transcriptZippingProgress || this.filesZippingProgress;
+    },
+    downloadOptions() {
+      return [
+        {
+          value: 'audio-recording',
+          text: this.$t('export.audioRecording', 2),
+          handler: this.callExportAudioFiles.bind(this),
+        },
+        {
+          value: 'video-recording',
+          text: this.$t('export.videoRecording', 2),
+          handler: this.callExportVideoFiles.bind(this),
+        },
+        {
+          value: 'screen-recording',
+          text: this.$t('export.screenRecording', 2),
+          handler: this.callExportScreenRecordings.bind(this),
+        },
+        {
+          value: 'transcript',
+          text: this.$t('export.transcription', 2),
+          handler: this.exportTranscripts.bind(this),
+        },
+      ];
+    },
+  },
+  created() {
+    this.initFilesExport({
+      fetchMethod: APIRepository.history.getHistory,
+      filename: 'history-records',
+      filesURL: getCallMediaUrl,
+    });
+  },
+  methods: {
+    callExportAudioFiles() {
+      if (this.isLoading) return;
+      const params = { ...this.filters, hasFile: true };
+      return this.exportFiles(null, params, EngineCallFileType.FileTypeAudio);
+    },
+    callExportVideoFiles() {
+      if (this.isLoading) return;
+      const params = { ...this.filters, hasFile: true };
+      return this.exportFiles(null, params, EngineCallFileType.FileTypeVideo);
+    },
+    callExportScreenRecordings() {
+      if (this.isLoading) return;
+      const params = { ...this.filters, hasFile: true };
+      return this.exportFiles(
+        null,
+        params,
+        EngineCallFileType.FileTypeScreensharing,
+      );
+    },
+  },
 };
 </script>
 
