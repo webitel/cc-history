0f16d8327b47c6e19eb2d6ebfdc65aca53a1b0e3
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-04-24 14:30:18 +0300

    refactor: replace TableMediaAction with WtCallMediaAction and remove unused code [WTEL-9334](https://webitel.atlassian.net/browse/WTEL-9334)

--- src/modules/main/modules/registry/components/history-registry.vue
+++ src/modules/main/modules/registry/components/history-registry.vue
@@ -156,15 +156,12 @@
         </template>
 
         <template #actions="{ item }">
-          <template v-if="recordingFiles(item).length">
-            <table-media-action
-              :currently-playing="currentlyMediaPlaying"
-              :files="recordingFiles(item)"
-              :icon="getRecordingTypeIcon(item)"
-              @play="handlePlayMedia"
-              @stop="closePlayer"
-            />
-          </template>
+          <wt-call-media-action
+            :currently-playing="currentlyMediaPlaying"
+            :files="item.files"
+            @play="handlePlayMedia"
+            @stop="closePlayer"
+          />
 
           <!--          v-if transcript can be added, exists, or already in progress -->
           <stt-action
@@ -228,10 +225,10 @@
 
 <script lang="ts" setup>
 import { getMediaUrl } from '@webitel/api-services/api';
-import { EngineCallFileType } from '@webitel/api-services/gen/models';
 import {
 	WtActionBar,
 	WtBadge,
+	WtCallMediaAction,
 	WtEmpty,
 	WtIconAction,
 	WtIconBtn,
@@ -261,7 +258,6 @@ import SttAction from '../modules/stt/components/registry/table-stt-action.vue';
 import { useRegistryStore } from '../store/new/registry.store.ts';
 import { WtScreenRecordingsAction } from '@webitel/ui-sdk/components';
 import TableDirection from './table-templates/table-direction.vue';
-import TableMediaAction from './table-templates/table-media-action.vue';
 import ScreenshotsAction from './table-templates/table-screenshots-action.vue';
 
 const emit = defineEmits<{
@@ -407,19 +403,6 @@ const setScreenRecording = (data) => {
 	currentScreenRecording.value.video = getMediaUrl(data.id);
 	closePlayer();
 	isScreenRecordingOpen.value = true;
-};
-
-const recordingFiles = (item) => {
-	return [
-		...(item.files?.[EngineCallFileType.FileTypeAudio] || []),
-		...(item.files?.[EngineCallFileType.FileTypeVideo] || []),
-	];
-};
-
-const getRecordingTypeIcon = (item) => {
-	return item.files?.[EngineCallFileType.FileTypeVideo]
-		? 'preview-tag-video'
-		: 'play';
 };
 
 const handlePlayMedia = (mediaData) => {
