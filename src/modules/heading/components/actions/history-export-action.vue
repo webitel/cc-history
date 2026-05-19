328c43d991c57c51a8c053abb2599a89f09a256d
volodymyr <volodymyr.dechtiaruk@gmail.com>
2026-05-19 14:37:12 +0300

    fix: fixing export filename and rewriting history-export-action.vue to composition api [WTEL-9575](https://webitel.atlassian.net/browse/WTEL-9575)

--- src/modules/heading/components/actions/history-export-action.vue
+++ src/modules/heading/components/actions/history-export-action.vue
@@ -57,182 +57,220 @@
   </div>
 </template>
 
-<script>
-import { computed } from 'vue';
+<script setup>
+import { computed, onMounted, reactive, ref, toRefs } from 'vue';
+import { useRoute } from 'vue-router';
 import { useVuelidate } from '@vuelidate/core';
 import { required, requiredIf } from '@vuelidate/validators';
 import TypesExportedSettingsEnum from '@webitel/ui-sdk/src/enums/TypesExportedSettings/TypesExportedSettings.enum';
-import exportCSVMixin from '@webitel/ui-sdk/src/modules/CSVExport/mixins/exportCSVMixin';
-import exportXLSMixin from '@webitel/ui-sdk/src/modules/CSVExport/mixins/exportXLSMixin';
+import { FormatDateMode } from '@webitel/ui-sdk/enums';
+import { formatDate } from '@webitel/ui-sdk/utils';
 import { EngineSystemSettingName } from 'webitel-sdk';
 import { SpecialGlobalAction } from '@webitel/ui-sdk/modules/Userinfo';
+import { useCSVExport } from '@webitel/ui-sdk/src/modules/CSVExport/composables/useCSVExport';
+import XLSExportClass from '@webitel/ui-sdk/src/modules/CSVExport/XLSExport';
 
 import APIRepository from '../../../../app/api/APIRepository';
 import ConfigurationAPI from '../../api/configuration.js';
-import historyActionMixin from '../../mixins/historyActionMixin';
 import FilesCounter from './files-counter.vue';
 import { useUserinfoStore } from '../../../userinfo/stores/userinfoStore';
+const props = defineProps({
+	dataList: {
+		type: Array,
+		default: () => [],
+	},
+	filters: {
+		type: Object,
+		default: () => ({}),
+	},
+	fields: {
+		type: Array,
+		required: true,
+	},
+	selected: {
+		type: Array,
+		default: () => [],
+	},
+});
+const { dataList, fields, filters, selected } = toRefs(props);
 
-export default {
-	name: 'HistoryExportAction',
-	components: {
-		FilesCounter,
-	},
-	mixins: [
-		historyActionMixin,
-		exportCSVMixin,
-		exportXLSMixin,
-	],
-	props: {
-		dataList: {
-			type: Array,
-		},
-		filters: {
-			type: Object,
-		},
-		fields: {
-			type: Array,
-			required: true,
-		},
-	},
-	validations() {
-		return {
-			draft: {
-				format: {
-					required,
-				},
-				separator: {
-					requiredIfRef: requiredIf(this.isExportSettingsFormatCSV),
-				},
-			},
-		};
-	},
-	setup: () => {
-		const userinfoStore = useUserinfoStore();
+const route = useRoute();
+const userinfoStore = useUserinfoStore();
 
-		const hasExportDataGridAccess = computed(() => {
-			return userinfoStore.hasSpecialGlobalActionAccess(
-				SpecialGlobalAction.ExportDataGrid,
-			);
-		});
+const exportPopup = ref(false);
+const isLoading = ref(false);
+const draft = reactive({
+	format: '',
+	separator: '',
+});
 
-		return {
-			v$: useVuelidate(),
-			hasExportDataGridAccess,
-		};
+const selectedIds = computed(() => selected.value.map((item) => item.id));
+
+const {
+	CSVExportInstance,
+	CSVDownloadProgress,
+	isCSVLoading,
+	initCSVExport,
+	exportCSV,
+} = useCSVExport({
+	selected: selectedIds,
+});
+
+const XLSExportInstance = ref(null);
+const XLSDownloadProgress = computed(() =>
+	XLSExportInstance.value ? XLSExportInstance.value.downloadProgress.count : 0,
+);
+const isXLSLoading = computed(() => !!XLSDownloadProgress.value);
+
+const CSVExport = computed(() => CSVExportInstance.value);
+const XLSExport = computed(() => XLSExportInstance.value);
+
+const hasExportDataGridAccess = computed(() => {
+	return userinfoStore.hasSpecialGlobalActionAccess(
+		SpecialGlobalAction.ExportDataGrid,
+	);
+});
+
+const exportSettingOptions = computed(() => {
+	return Object.keys(TypesExportedSettingsEnum).map((key) => ({
+		name: TypesExportedSettingsEnum[key],
+		value: TypesExportedSettingsEnum[key],
+		id: TypesExportedSettingsEnum[key],
+	}));
+});
+
+const isExportSettingsFormatCSV = computed(
+	() => draft.format === TypesExportedSettingsEnum.CSV,
+);
+
+const rules = computed(() => ({
+	draft: {
+		format: {
+			required,
+		},
+		separator: {
+			requiredIfRef: requiredIf(() => isExportSettingsFormatCSV.value),
+		},
 	},
-	data: () => ({
-		exportPopup: false,
-		isLoading: false,
-		draft: {
-			format: {},
-			separator: '',
-		},
-	}),
-	computed: {
-		selectedIds() {
-			return this.selected.map(({ id }) => id);
-		},
-		exportSettingOptions() {
-			return Object.keys(TypesExportedSettingsEnum).map((key) => ({
-				name: TypesExportedSettingsEnum[key],
-				value: TypesExportedSettingsEnum[key],
-				id: TypesExportedSettingsEnum[key],
-			}));
-		},
-		isExportSettingsFormatCSV() {
-			return this.draft?.format === TypesExportedSettingsEnum.CSV;
-		},
-		disableSaving() {
-			this.v$.draft.$touch();
-			return this.v$.draft.$pending || this.v$.draft.$error;
-		},
-	},
-	mounted() {
-		this.checkExportSettings();
-	},
-	created() {
-		this.initCSVExport(APIRepository.history.exportHistoryToFile, {
-			filename: 'history',
-		});
-		this.initXLSExport(APIRepository.history.exportHistoryToFile, {
-			filename: 'history',
-		});
-	},
-	methods: {
-		updateDraft({ format, separator } = {}) {
-			this.draft = {
-				format: format || '',
-				separator: separator || '',
-			};
-		},
-		async checkExportSettings() {
-			const response = await ConfigurationAPI.getList({
-				name: EngineSystemSettingName.ExportSettings,
+}));
+const v$ = useVuelidate(rules, {
+	draft,
+});
+
+const disableSaving = computed(() => {
+	v$.value.draft.$touch();
+	return v$.value.draft.$pending || v$.value.draft.$error;
+});
+
+function initXLSExport(fetchMethod, options = {}) {
+	XLSExportInstance.value = new XLSExportClass(fetchMethod, options);
+}
+
+async function exportXLS(exportParams) {
+	const routeQuery = route.query;
+	const params = {
+		...(exportParams || routeQuery),
+		size: 5000,
+	};
+	if (selectedIds.value.length) {
+		params.id = selectedIds.value;
+	}
+	await XLSExportInstance.value?.export(params);
+}
+
+function updateDraft({ format, separator } = {}) {
+	draft.format = format || '';
+	draft.separator = separator || '';
+}
+
+async function checkExportSettings() {
+	const response = await ConfigurationAPI.getList({
+		name: EngineSystemSettingName.ExportSettings,
+	});
+	const exportSettingsValue = response.items[0]?.value;
+
+	if (exportSettingsValue) {
+		updateDraft(exportSettingsValue);
+	}
+}
+
+function getExportFilename() {
+	const date = formatDate(Date.now(), FormatDateMode.DATE);
+	const time = formatDate(Date.now(), FormatDateMode.TIME_SEC);
+	return `history-${date}-${time}`;
+}
+
+function exportFile(format) {
+	const delimiter = draft.separator;
+	const filename = getExportFilename();
+	// https://webitel.atlassian.net/browse/DEV-3797
+	const params = {
+		...filters.value,
+		fields: fields.value,
+		skipParent: true,
+		_columns: fields.value,
+	};
+
+	if (CSVExport.value) {
+		CSVExport.value.filename = filename;
+	}
+	if (XLSExport.value) {
+		XLSExport.value.filename = filename;
+	}
+
+	switch (format) {
+		case TypesExportedSettingsEnum.CSV:
+			return exportCSV({
+				...params,
+				delimiter,
 			});
-			const exportSettingsValue = response.items[0]?.value;
+		case TypesExportedSettingsEnum.XLSX:
+			return exportXLS(params);
+		default:
+			console.error(`Unsupported format: ${format}`);
+	}
+}
 
-			if (exportSettingsValue) {
-				this.updateDraft(exportSettingsValue);
-			}
-		},
-		exportFile(format) {
-			const fields = this.fields;
-			const delimiter = this.draft.separator;
-			// https://webitel.atlassian.net/browse/DEV-3797
-			const params = {
-				...this.filters,
-				fields,
-				skipParent: true,
-				_columns: fields,
-			};
-			switch (format) {
-				case TypesExportedSettingsEnum.CSV:
-					return this.exportCSV({
-						...params,
-						delimiter,
-					});
-				case TypesExportedSettingsEnum.XLSX:
-					return this.exportXLS(params);
-				default:
-					console.error(`Unsupported format: ${format}`);
-			}
-		},
-		handleExport() {
-			if (!this.draft.format?.length) {
-				this.exportPopup = true;
-			} else {
-				this.exportFile(this.draft.format);
-			}
-		},
-		save() {
-			this.isLoading = true;
-			try {
-				this.exportFile(this.draft.format);
-			} finally {
-				this.isLoading = false;
-				this.closePopup();
-			}
-			//NOTE: This code is required to clear draft and re-execute checkExportSettings
-			this.updateDraft();
-		},
-		selectHandler(selectedValue) {
-			this.draft.format = selectedValue.value;
-			if (!this.isExportSettingsFormatCSV) {
-				delete this.draft.separator;
-			}
-		},
-		inputHandler(inputValue) {
-			this.draft.separator = inputValue;
-		},
-		closePopup() {
-			this.exportPopup = false;
+function handleExport() {
+	if (!draft.format?.length) {
+		exportPopup.value = true;
+	} else {
+		exportFile(draft.format);
+	}
+}
 
-			//NOTE: This code is required to clear draft and re-execute checkExportSettings
-			this.updateDraft();
-		},
-	},
-};
+function closePopup() {
+	exportPopup.value = false;
+	// NOTE: this is required to clear draft and re-execute checkExportSettings
+	updateDraft();
+}
+
+function save() {
+	isLoading.value = true;
+	try {
+		exportFile(draft.format);
+	} finally {
+		isLoading.value = false;
+		closePopup();
+	}
+	// NOTE: this is required to clear draft and re-execute checkExportSettings
+	updateDraft();
+}
+
+function selectHandler(selectedValue) {
+	draft.format = selectedValue.value;
+	if (!isExportSettingsFormatCSV.value) {
+		draft.separator = '';
+	}
+}
+
+initCSVExport(APIRepository.history.exportHistoryToFile, {
+	filename: 'history',
+});
+initXLSExport(APIRepository.history.exportHistoryToFile, {
+	filename: 'history',
+});
+onMounted(checkExportSettings);
 </script>
 
 <style lang="scss" scoped>
