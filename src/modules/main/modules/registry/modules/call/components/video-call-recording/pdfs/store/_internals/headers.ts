4b01fb54eb38915a3d4e1e05d22ec957dedd7949
Yehor Hlukhov <egorgluhov68@gmail.com>
2026-08-13 18:44:03 +0300

    fix: remove sorting from pdfs table [WTEL-10154](https://webitel.atlassian.net/browse/WTEL-10154)

--- src/modules/main/modules/registry/modules/call/components/video-call-recording/pdfs/store/_internals/headers.ts
+++ src/modules/main/modules/registry/modules/call/components/video-call-recording/pdfs/store/_internals/headers.ts
@@ -9,28 +9,24 @@ export const headers = [
 		value: 'name',
 		locale: 'reusable.name',
 		show: true,
-		sort: null,
 		field: 'name',
 	},
 	{
 		value: 'status',
 		locale: 'reusable.status',
 		show: true,
-		sort: null,
 		field: 'status',
 	},
 	{
 		value: 'created_at',
 		locale: 'reusable.createdAt',
 		show: true,
-		sort: null,
 		field: 'created_at',
 	},
 	{
 		value: 'created_by',
 		locale: 'reusable.createdBy',
 		show: true,
-		sort: null,
 		field: 'created_by',
 	},
 ];
